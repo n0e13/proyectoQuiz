e58a0520c567c39f0233c5b2bd5ada0eb66433e7
n0e13 <noemy.garcia@gmail.com>
2022-02-15 16:52:56 +0100

    en blanco

---
+++ js/script.js
@@ -0,0 +1,147 @@
+const preguntas = [
+    {
+        titulo: '¿Qué cordillera separa Europa de Asia?',
+        respuestas: [
+            {
+                class: 'btn_red',
+                type: 'button',
+                name: 'no',
+                value: 'Despeñaperros'
+            },
+            {
+                class: 'btn_orange',
+                type: 'button',
+                name: 'no',
+                value: 'Caucaso'
+            },
+            {
+                class: 'btn_blue',
+                type: 'button',
+                name: 'yes',
+                value: 'Urales'
+            },
+            {
+                class: 'btn_green',
+                type: 'button',
+                name: 'no',
+                value: 'Apeninos'
+            }
+        ]
+    },
+    {
+        titulo: '¿Quiénes fueron, según la leyenda, los dos hermanos fundadores de la ciudad de Roma?',
+        respuestas: [
+            {
+                class: 'btn_orange',
+                type: 'button',
+                name: 'no',
+                value: 'Ramón El Vanidoso'
+            },
+            {
+                class: 'btn_red',
+                type: 'button',
+                name: 'no',
+                value: 'Romualdo y Remo'
+            },
+            {
+                class: 'btn_green',
+                type: 'button',
+                name: 'no',
+                value: 'Rémulo y romo'
+            },
+            {
+                class: 'btn_blue',
+                type: 'button',
+                name: 'yes',
+                value: 'Rómulo y Remo'
+            }
+        ]
+    },
+    {
+        titulo: '¿Cuál es la velocidad de la luz?',
+        respuestas: [
+            {
+                class: 'btn_blue',
+                type: 'button',
+                name: 'yes',
+                value: '300.000km/s'
+            },
+            {
+                class: 'btn_green',
+                type: 'button',
+                name: 'no',
+                value: '300.000 km/h'
+            },
+            {
+                class: 'btn_red',
+                type: 'button',
+                name: 'no',
+                value: '50 km/h'
+            },
+            {
+                class: 'btn_orange',
+                type: 'button',
+                name: 'no',
+                value: '300.000.000 km/h'
+            }
+        ]
+    },
+    {
+        titulo: '¿Quién escribió “La colmena”?',
+        respuestas: [
+            {
+                class: 'btn_red',
+                type: 'button',
+                name: 'no',
+                value: 'Miguel de Cervantes'
+            },
+            {
+                class: 'btn_blue',
+                type: 'button',
+                name: 'no',
+                value: 'La abeja Maya'
+            },
+            {
+                class: 'btn_orange',
+                type: 'button',
+                name: 'yes',
+                value: 'Camilo José Cela'
+            },
+            {
+                class: 'btn_green',
+                type: 'button',
+                name: 'no',
+                value: 'Gabriel García Márquez'
+            }
+        ]
+    },
+    {
+        titulo: '¿Cómo se llama el protagonista de la saga de videojuegos "The Legend of Zelda"?',
+        respuestas: [
+            {
+                class: 'btn_blue',
+                type: 'button',
+                name: 'yes',
+                value: 'Link'
+            },
+            {
+                class: 'btn_green',
+                type: 'button',
+                name: 'no',
+                value: 'Luis'
+            },
+            {
+                class: 'btn_red',
+                type: 'button',
+                name: 'no',
+                value: 'Zelda'
+            },
+            {
+                class: 'btn_orange',
+                type: 'button',
+                name: 'no',
+                value: 'Lucas'
+            }
+        ]
+    }
+]
