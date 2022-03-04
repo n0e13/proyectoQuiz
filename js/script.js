1691ac86c27af3134acfc7b0d8126ae7bc03735a
n0e <noemy.garcia@gmail.com>
2022-03-04 10:31:55 +0100

    [UPDATE] Show info

--- js/script.js
+++ js/script.js
@@ -499,11 +499,12 @@ auth.onAuthStateChanged(user => {
 // ************************** //
 
 function showUserData(nick, email, photo) {
-    userData.style.cssText = 'background-color: #73AB84;width: 50%;margin: 2rem auto;padding: 1rem;border-radius: 5px;display: flex;flex-direction: column;align-items: center';
+    userData.style.cssText = 'background-color: #73AB84;width: 50%;margin: 2rem auto;padding: 1rem;border-radius: 5px;display: flex;flex-direction: column;align-items: center; visibility:visible';
     userData.innerHTML = `<h3>User Data</h3>
                   <p>Username: ${nick}</p>
-                  <p>Email: ${email}</p>
-                  <img src=${photo} alt='User profile picture'>`
+                  <img src=${photo} alt='User profile picture'>
+                 <p>Responde rápido si quieres que se te recuerde.</p>
+                 <p> Calienta motores. Has venido a jugar. <a href="./pages/question.html">¡JUEGA!</a></p>`      
 }
 
 
@@ -566,46 +567,8 @@ async function saveInDB(nick) {
     await setDoc(doc(usersRef, userGoogle.email), {
         username: nick,
     }).then(() => {
-        showUserData(nick,userGoogle.email, userGoogle.photoURL);
+        showUserData(nick, userGoogle.email, userGoogle.photoURL);
         console.log("Usuario guardado");
-    });
-}
-
-
-
-
-// *********************************************** //
-//                                                 //
-//   //
-//                                                 //
-// *********************************************** //
-
-if (document.getElementById('start') != null) {
-    document.getElementById('start').addEventListener('click', function (event) {
-        // Si metió un nombre y mostrar las reglas del juego y el enlace
-        // TODO: Una vez metido el nombre, comprobar si existe en la bd para mostrar su gráfica de juego anterior
-        playerName = document.getElementById('player').value;
-
-        if (playerName != "") {
-            let bio = document.getElementById("bio_game");
-            bio.innerHTML = `<p>¡Hola, <strong>${playerName}</strong>!</p><p>Aquí tienes que darte vida para aparecer en la lista.</p>Las reglas son las siguientes: 
-        <ul>
-        <li>Tienes 5 segundos para leer y contestar cada pregunta.</li>
-        <li>La puntuación es el sumatorio de los segundos que te sobran en cada pregunta.</li>
-        <li>Hay 10 cuestiones diferentes. Se muestran aleatoriamente.</li>
-        <li>Cada pregunta perdida o fallada resta 1 punto.</li>
-        <li>Cada partida dura 5 preguntas.</li>
-        </ul>
-        <p><p> Calienta motoros. Has venido a jugar. <a href="./pages/question.html">¡JUEGA!</a>`;
-            bio.style.display = "block";
-            bio.style.visibility = "visible";
-        } else { // Aviso de que falta un nombre 
-            let bio = document.getElementById("bio_game");
-            bio.innerHTML = `Debes introducir un nombre para jugar`;
-            bio.style = "border: 2px solid red; border-radius: 25px; padding: 10px;";
-            bio.style.display = "block";
-            bio.style.visibility = "visible";
-        }
     });
 }
 
